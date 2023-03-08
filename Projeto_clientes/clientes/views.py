4623768b015eecfbea7a896862656a3a48c5a00e
Antônio Matheus Follone Graça <matheus-follone@follone.com.br>
2023-03-07 23:59:58 -0300

    Busca pelo campo nome,cpf e usuários ativos
    Busca pelo campo nome,cpf e usuários ativos

--- Projeto_clientes/clientes/views.py
+++ Projeto_clientes/clientes/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets,filters
+from rest_framework import viewsets
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
+from rest_framework import filters
 
 import django_filters.rest_framework
 
@@ -11,8 +12,13 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    # ordenando pelo campo nome
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.OrderingFilter]
+    # ordenando pelo campo nome e busca por nome
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    # ordenação pelo campo nome
     ordering_fields = ['nome']
+    # busca pelo campo nome e cpf
+    search_fields = ['nome','cpf']
+    # filtrando por campo ativo
+    filterset_fields = ['ativo']
    
 
